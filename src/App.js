f3483b7ee08edfb927ea3b85e65e1cacbc6300f3
Thomas <tholmes59@gmail.com>
2020-05-19 10:58:41 -0700

    add and test page cpntainer to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import PageContainer from './components/PageContainer'
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <PageContainer/>
     </div>
   );
 }
