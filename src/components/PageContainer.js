93635a65565a4de70c93c31ce3eb153486c93c10
Thomas <tholmes59@gmail.com>
2020-05-19 10:49:32 -0700

    create page container component

---
+++ src/components/PageContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const PageContainer = () => {
+
+    return (
+        <div>
+            Contains form and display
+        </div>
+    )
+}
+
+export default PageContainer;
