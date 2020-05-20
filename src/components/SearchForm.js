d9cd18ff9c566482231c112e931dec6ff72f9855
Thomas <tholmes59@gmail.com>
2020-05-20 16:10:47 -0700

    create form to enter city and country on search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 
-const SearchForm = () => {
+const SearchForm = (props) => {
+    
 
         return(
             <div>
                 Form
+                <form onSubmit={props.getWeatherInfo}>
+                    <input type='text' placeholder="Paris" name='city' />
+                    <input type="text" placeholder='Fr' name="country" />
+                    <input value="Search" type="submit" />
+                </form>
             </div>
         )
 }
