9457928477a06b19890425872364a252dd52c01e
Thomas <tholmes59@gmail.com>
2020-05-20 16:09:52 -0700

    add form and weather display to app.js

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import SearchForm from './SearchForm'
+import WeatherDisplay from './WeatherDisplay'
 
 
 const PageContainer = () => {
-
+   
     return (
         <div>
             Contains form and display
+            <SearchForm  />
+            <WeatherDisplay/>
         </div>
     )
 }
