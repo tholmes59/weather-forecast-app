259a6f341c10669a309be0013302d7a330f94b16
Thomas <tholmes59@gmail.com>
2020-05-22 10:30:43 -0700

    add async api call function

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState} from 'react';
 import SearchForm from './SearchForm'
 import WeatherDisplay from './WeatherDisplay'
 
 
+
 const PageContainer = () => {
    
+    const [weather, setWeather] = useState([])
+    const API_KEY = process.env.REACT_APP_APIKEY;
+
+    async function fetchWeatherData(e) {
+        e.preventDefault()
+        const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`)
+        .then(res => res.json())
+        .then(data => data)
+        setWeather({
+            data: weatherData
+        })
+    }
+
     return (
         <div>
             Contains form and display
-            <SearchForm  />
-            <WeatherDisplay/>
+            <SearchForm  getWeatherInfo={fetchWeatherData}/>
+           
         </div>
     )
 }
