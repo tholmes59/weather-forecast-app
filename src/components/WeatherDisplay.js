d6c8f573dfe4223e54d26084971b035c8f987bdd
Thomas <tholmes59@gmail.com>
2020-05-26 16:46:21 -0700

    add descriptors to weather data

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -6,10 +6,10 @@ const WeatherDisplay = (props) => {
     return (
         <div>
             {console.log(props)}
-            <p>{props.weather && props.weather.name + ', ' + props.weather.sys.country}</p>
-            <p>{props.weather && props.weather.main.temp}</p>
-            <p>{props.weather && props.weather.main.feels_like}</p>
-            <p>{props.weather && props.weather.humidity}</p>
+            <p>Location: {props.weather && props.weather.name + ', ' + props.weather.sys.country}</p>
+            <p>Temp: {props.weather && props.weather.main.temp}</p>
+            <p>Feels Like:{props.weather && props.weather.main.feels_like}</p>
+            <p>Humidity: {props.weather && props.weather.main.humidity}</p>
             
         </div>
     )
