7c9d2dc2763a32c4e2109fd2d10f96ff47db1df0
Thomas <tholmes59@gmail.com>
2020-05-27 13:32:13 -0700

    add max temp to weather display

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,13 +3,15 @@ import React from 'react'
 
 const WeatherDisplay = (props) => {
 
+
     return (
         <div>
             {console.log(props)}
             <p>Location: {props.weather && props.weather.name + ', ' + props.weather.sys.country}</p>
             <p>Temp: {props.weather && props.weather.main.temp}</p>
-            <p>Feels Like:{props.weather && props.weather.main.feels_like}</p>
+            <p>Feels Like: {props.weather && props.weather.main.feels_like}</p>
             <p>Humidity: {props.weather && props.weather.main.humidity}</p>
+            <p>Max Temp: {props.weather && props.weather.main.temp_max}</p>
             
         </div>
     )
