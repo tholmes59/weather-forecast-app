3d524061ba701659f95bc3946ebc8c3030d0a2c4
Thomas <tholmes59@gmail.com>
2020-06-07 06:51:12 -0700

    convernt max temp to F

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -11,7 +11,7 @@ const WeatherDisplay = (props) => {
             <p>Temp: {props.weather && (props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F/{props.weather && (props.weather.main.temp - 273.15).toFixed(0)}°C</p>
             <p>Feels Like: {props.weather && (props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F</p>
             <p>Humidity: {props.weather && props.weather.main.humidity}</p>
-            <p>Max Temp: {props.weather && props.weather.main.temp_max}</p>
+            <p>Max Temp: {props.weather && (props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}</p>
             <p>Min Temp: {props.weather && props.weather.main.temp_min}</p>
             <p>Wind: {props.weather && props.weather.wind.speed}</p>
             <p>Gusts: {props.weather && props.weather.wind.gust}</p>
