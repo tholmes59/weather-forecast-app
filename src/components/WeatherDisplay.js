35d83fe261fd8cad681747acd474f2802c111aa1
Thomas <tholmes59@gmail.com>
2020-06-08 09:57:41 -0700

    add converted feels like temp in celcius

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -9,7 +9,7 @@ const WeatherDisplay = (props) => {
             {console.log(props)}
             <p>Location: {props.weather && props.weather.name + ', ' + props.weather.sys.country}</p>
             <p>Temp: {props.weather && (props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F/{props.weather && (props.weather.main.temp - 273.15).toFixed(0)}°C</p>
-            <p>Feels Like: {props.weather && (props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F</p>
+            <p>Feels Like: {props.weather && (props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>
             <p>Humidity: {props.weather && props.weather.main.humidity}</p>
             <p>Max Temp: {props.weather && (props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp_max - 273.15).toFixed(0)}°C</p>
             <p>Min Temp: {props.weather && props.weather.main.temp_min}</p>
