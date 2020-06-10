221e931343c225014ea8468e340c19d36ff0b23d
Thomas <tholmes59@gmail.com>
2020-06-10 10:05:54 -0700

    convernt min temp to Celcius

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -12,7 +12,7 @@ const WeatherDisplay = (props) => {
             <p>Feels Like: {props.weather && (props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>
             <p>Humidity: {props.weather && props.weather.main.humidity}</p>
             <p>Max Temp: {props.weather && (props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp_max - 273.15).toFixed(0)}°C</p>
-            <p>Min Temp: {props.weather && (props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F</p>
+            <p>Min Temp: {props.weather && (props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp_min  - 273.15).toFixed(0)}°C</p>
             <p>Wind: {props.weather && props.weather.wind.speed}</p>
             <p>Gusts: {props.weather && props.weather.wind.gust}</p>
             <p>Description {props.weather && props.weather.weather[0].description}</p>
