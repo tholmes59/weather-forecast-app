9d7227ec83e9a11a2ba766e97a98440d7e9fe483
Thomas <tholmes59@gmail.com>
2020-06-17 13:49:11 -0700

    change position of weather image

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -26,7 +26,7 @@ const WeatherDisplay = (props) => {
         <div>
             {console.log(props)}
             <p>Location: {props.weather && props.weather.name + ', ' + props.weather.sys.country}</p>
-            <p>Temp: {props.weather && (props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp - 273.15).toFixed(0)}°C</p>
+            <p>Temp: {props.weather && (props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp - 273.15).toFixed(0)}°C</p> <img src={image} alt=""/>
             <p>Feels Like: {props.weather && (props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>
             <p>Humidity: {props.weather && props.weather.main.humidity}%</p>
             <p>Max Temp: {props.weather && (props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp_max - 273.15).toFixed(0)}°C</p>
@@ -35,7 +35,7 @@ const WeatherDisplay = (props) => {
             <p>Direction: {props.weather && degToCompass(props.weather.wind.deg)}</p>
             <p>Gusts: {props.weather && (props.weather.wind.gust * 2.237).toFixed(0)} mph</p>
             <p>Description {props.weather && props.weather.weather[0].description}</p>
-            <img src={image} alt=""/>
+            {/* <img src={image} alt=""/> */}
             {weatherImg}
         </div>
     )
