7aba5ceaec3c9ec059bc217b62267b4d375ca90e
Thomas <tholmes59@gmail.com>
2020-06-22 11:51:26 -0700

    rearrange temp display order

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -32,9 +32,9 @@ const WeatherDisplay = (props) => {
             <p>Location: {props.weather && props.weather.name + ', ' + props.weather.sys.country}</p>
             <p>Temp: {props.weather && (props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp - 273.15).toFixed(0)}°C</p> <img src={image} alt=""/>
             <p>Feels Like: {props.weather && (props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>
-            <p>Humidity: {props.weather && props.weather.main.humidity}%</p>
             <p>Max Temp: {props.weather && (props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp_max - 273.15).toFixed(0)}°C</p>
             <p>Min Temp: {props.weather && (props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp_min  - 273.15).toFixed(0)}°C</p>
+            <p>Humidity: {props.weather && props.weather.main.humidity}%</p>
             <p>Wind: {props.weather && (props.weather.wind.speed * 2.237).toFixed(0)} mph</p>
             <p>Direction: {props.weather && degToCompass(props.weather.wind.deg)}</p>
             <p>Gusts: {props.weather && (props.weather.wind.gust * 2.237).toFixed(0)} mph</p>
