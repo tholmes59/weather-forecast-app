98e79d82fcc6cb7c306e37da2ca5648fb1acc649
Thomas <tholmes59@gmail.com>
2020-06-23 11:18:46 -0700

    remove F temps from C component

--- src/components/Celsius.js
+++ src/components/Celsius.js
@@ -7,10 +7,10 @@ const Celsius = (props) => {
 
     return(
         <div>
-            <p>Temp: {props.weather && (props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp - 273.15).toFixed(0)}°C</p> <img src={image} alt=""/>
-            <p>Feels Like: {props.weather && (props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>
-            <p>Max Temp: {props.weather && (props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp_max - 273.15).toFixed(0)}°C</p>
-            <p>Min Temp: {props.weather && (props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F / {props.weather && (props.weather.main.temp_min  - 273.15).toFixed(0)}°C</p>
+            <p>Temp: {props.weather && (props.weather.main.temp - 273.15).toFixed(0)}°C</p> <img src={image} alt=""/>
+            <p>Feels Like: {props.weather && (props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>
+            <p>Max Temp: {props.weather && (props.weather.main.temp_max - 273.15).toFixed(0)}°C</p>
+            <p>Min Temp: {props.weather && (props.weather.main.temp_min  - 273.15).toFixed(0)}°C</p>
         </div>
     )
 }
