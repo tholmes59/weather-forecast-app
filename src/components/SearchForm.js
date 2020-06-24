92b9d447e7bf6122b5aeed806a7f10086157007d
Thomas <tholmes59@gmail.com>
2020-06-24 10:14:18 -0700

    change class to classname

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,7 +5,7 @@ const SearchForm = (props) => {
     
 
         return(
-            <div class="search-form">
+            <div className="search-form">
                 <form onSubmit={props.getWeatherInfo}>
                     <input type='text' placeholder="Paris" name='city' /><br/>
                     <input type="text" placeholder='Fr' name="country" /><br/>
