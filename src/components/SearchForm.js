e663db70da203e254d49ab6e55c05257dd8b1f44
Thomas <tholmes59@gmail.com>
2020-06-29 15:18:25 -0700

    add classnames to searchform inputs

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,8 +7,8 @@ const SearchForm = (props) => {
         return(
             <div className="search-form">
                 <form onSubmit={props.getWeatherInfo}>
-                    <input type='text' placeholder="Enter city..." name='city' />
-                    <input type="text" placeholder='Enter country...' name="country" /><br/>
+                    <input type='text' placeholder="Enter city..." name='city' className='city'/>
+                    <input type="text" placeholder='Enter country...' name="country" className='country'/><br/>
                     <input value="Search" type="submit" />
                 </form>
             </div>
