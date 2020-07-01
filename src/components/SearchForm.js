26ea80709a2abeecca964e8dae3ab2d9b05fca2a
Thomas <tholmes59@gmail.com>
2020-07-01 11:18:54 -0700

    add required to input fields

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 
 const SearchForm = (props) => {
-    
-
+   
         return(
             <div className="search-form">
                 <form onSubmit={props.getWeatherInfo}>
-                    <input type='text' placeholder="Enter city..." name='city' className='city'/>
-                    <input type="text" placeholder='Enter country...' name="country" className='country'/><br/>
+                    <input type='text' placeholder="Enter city..." name='city' className='city'  required/>
+                    <input type="text" placeholder='Enter country...' name="country" className='country' required/><br/>
                     <button value="Search" type="submit">Search</button>
                 </form>
             </div>
