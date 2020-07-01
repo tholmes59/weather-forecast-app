4959eca84f2ba8d7acce495d1ad8b9201b2fbbd5
Thomas <tholmes59@gmail.com>
2020-07-01 11:58:44 -0700

    remove required from input fields and replace it with error handling through hooks

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,8 +6,8 @@ const SearchForm = (props) => {
         return(
             <div className="search-form">
                 <form onSubmit={props.getWeatherInfo}>
-                    <input type='text' placeholder="Enter city..." name='city' className='city'  required/>
-                    <input type="text" placeholder='Enter country...' name="country" className='country' required/><br/>
+                    <input type='text' placeholder="Enter city..." name='city' className='city'  />
+                    <input type="text" placeholder='Enter country...' name="country" className='country' /><br/>
                     <button value="Search" type="submit">Search</button>
                 </form>
             </div>
