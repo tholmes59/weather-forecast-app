d16d6ecd10e40455e0dee9cb0ce6ea8c0fcf72cb
Thomas <tholmes59@gmail.com>
2020-07-06 11:51:38 -0700

    change layout of weather display

--- src/components/Fahrenheit.js
+++ src/components/Fahrenheit.js
@@ -10,7 +10,7 @@ const Fahrenheit = (props) => {
             {props.weather &&  <p>Temp: {(props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F</p>} <img src={image} alt=""/>
             {props.weather && <p>Feels Like: {(props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F</p>}
             {props.weather && <p>Max Temp: {(props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F</p>}
-            <p>Min Temp: {props.weather && (props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F</p>
+            {props.weather && <p>Min Temp: {(props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F</p>}
         </div>
     )
 }
