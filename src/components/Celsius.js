ca59fd7fa9dc20c7f3517f0a95129d0758293d23
Thomas <tholmes59@gmail.com>
2020-07-06 11:52:49 -0700

    change layout of weather display

--- src/components/Celsius.js
+++ src/components/Celsius.js
@@ -7,7 +7,7 @@ const Celsius = (props) => {
 
     return(
         <div>
-            <p>Temp: {props.weather && (props.weather.main.temp - 273.15).toFixed(0)}°C</p> <img src={image} alt=""/>
+            {props.weather && <p>Temp: {(props.weather.main.temp - 273.15).toFixed(0)}°C</p>} <img src={image} alt=""/>
             <p>Feels Like: {props.weather && (props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>
             <p>Max Temp: {props.weather && (props.weather.main.temp_max - 273.15).toFixed(0)}°C</p>
             <p>Min Temp: {props.weather && (props.weather.main.temp_min  - 273.15).toFixed(0)}°C</p>
