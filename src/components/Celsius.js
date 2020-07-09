581a815c44d4430316bcaf07273fb43a87ce4f60
Thomas <tholmes59@gmail.com>
2020-07-09 10:53:26 -0700

    add classname to component

--- src/components/Celsius.js
+++ src/components/Celsius.js
@@ -6,7 +6,7 @@ const Celsius = (props) => {
     let image = `http://openweathermap.org/img/wn/${imageId}@2x.png`
 
     return(
-        <div>
+        <div className="fahrenheit-celsius-container">
             {props.weather && <p>Temp: {(props.weather.main.temp - 273.15).toFixed(0)}°C</p>} <img src={image} alt=""/>
             {props.weather && <p>Feels Like: {(props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>}
             {props.weather && <p>Max Temp: {(props.weather.main.temp_max - 273.15).toFixed(0)}°C</p>}
