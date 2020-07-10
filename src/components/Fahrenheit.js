b3dab5f63d8864092983e5a26471a469cc84a9b0
Thomas <tholmes59@gmail.com>
2020-07-10 11:43:21 -0700

    work on layout of F/C components

--- src/components/Fahrenheit.js
+++ src/components/Fahrenheit.js
@@ -7,7 +7,7 @@ const Fahrenheit = (props) => {
 
     return(
         <div className="fahrenheit-celsius-container">
-            {props.weather &&  <p>Temp: {(props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F</p>} <img src={image} alt=""/>
+            {props.weather && <span>Temp: {(props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F</span>} <img src={image} alt=""/>
             {props.weather && <p>Feels Like: {(props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F</p>}
             {props.weather && <p>Max Temp: {(props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F</p>}
             {props.weather && <p>Min Temp: {(props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F</p>}
