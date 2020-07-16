0de9babecb06562c0632d90f77e63c8dbe158907
Thomas <tholmes59@gmail.com>
2020-07-16 08:15:03 -0700

    change layout of F/C

--- src/components/Celsius.js
+++ src/components/Celsius.js
@@ -7,9 +7,13 @@ const Celsius = (props) => {
 
     return(
         <div className="fahrenheit-celsius-container">
-            {props.weather && <span>Temp: {(props.weather.main.temp - 273.15).toFixed(0)}°C</span>} <img src={image} alt=""/>
+            <div className="max-min-container">
+                {props.weather && <span>Max Temp: {(props.weather.main.temp_max - 273.15).toFixed(0)}°C |</span>} {props.weather && <span>Min Temp: {(props.weather.main.temp_min  - 273.15).toFixed(0)}°C</span>}
+            </div>
+            {props.weather && <span>Temp: {(props.weather.main.temp - 273.15).toFixed(0)}°C</span>} 
+            {/* <img src={image} alt=""/> */}
             {props.weather && <p>Feels Like: {(props.weather.main.feels_like - 273.15).toFixed(0)}°C</p>}
-            {props.weather && <span>Max Temp: {(props.weather.main.temp_max - 273.15).toFixed(0)}°C |</span>} {props.weather && <span>Min Temp: {(props.weather.main.temp_min  - 273.15).toFixed(0)}°C</span>}
+            
             
         </div>
     )
