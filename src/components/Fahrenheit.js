4d7af59e17870b637e60d0d572168b98060ad324
Thomas <tholmes59@gmail.com>
2020-07-20 10:50:19 -0700

    fix fahrenheit container to only render full div when data is fetched

--- src/components/Fahrenheit.js
+++ src/components/Fahrenheit.js
@@ -6,15 +6,17 @@ const Fahrenheit = (props) => {
     let image = `http://openweathermap.org/img/wn/${imageId}@2x.png`
 
     return(
-        <div className="fahrenheit-celsius-container">
-            <div className="max-min-container">
-                {props.weather && <span>Max Temp: {(props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F |</span>} {props.weather && <span>Min Temp: {(props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F</span>}
-            </div>
-            {props.weather && <span>Temp: {(props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F </span>} 
-            {/* <img src={image} alt=""/> */}
-            {props.weather && <p>Feels Like: {(props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F</p>}
-            
-            
+        <div>
+            {props.weather && 
+                <div className="fahrenheit-celsius-container">
+                    <div className="max-min-container">
+                        {props.weather && <span>Max Temp: {(props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F |</span>} {props.weather && <span>Min Temp: {(props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F</span>}
+                    </div>
+                    {props.weather && <span>Temp: {(props.weather.main.temp * (9/5)-459.67).toFixed(0)}°F </span>} 
+                    {/* <img src={image} alt=""/> */}
+                    {props.weather && <p>Feels Like: {(props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F</p>}
+                </div>
+            }
         </div>
     )
 }
