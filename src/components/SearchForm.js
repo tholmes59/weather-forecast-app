3917f0d3c804ded2932345c70fbaaedeb5feb495
Thomas <tholmes59@gmail.com>
2020-09-15 11:23:15 -0700

    add input container classname

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,9 +6,11 @@ const SearchForm = (props) => {
         return(
             <div className="search-form">
                 <form onSubmit={props.getWeatherInfo}>
-                    <input type='text' placeholder="Enter city..." name='city' className='city'  />
-                    <input type="text" placeholder='Enter country...' name="country" className='country' /><br/>
-                    <button value="Search" type="submit">Search</button>
+                    <div className="input-container">
+                        <input type='text' placeholder="Enter city..." name='city' className='city'  />
+                        <input type="text" placeholder='Enter country...' name="country" className='country' /><br/>
+                        <button value="Search" type="submit">Search</button>
+                    </div>
                 </form>
             </div>
         )
