9c68a14cbcffcbdb1d6145c5671a62b76c946100
Thomas <tholmes59@gmail.com>
2020-09-30 09:54:40 -0700

    update placeholders for search fields

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,8 +7,8 @@ const SearchForm = (props) => {
             <div className="search-form">
                 <form onSubmit={props.getWeatherInfo}>
                     <div className="input-container">
-                        <input type='text' placeholder="Enter city..." name='city' className='city'  />
-                        <input type="text" placeholder='Enter country...' name="country" className='country' /><br/>
+                        <input type='text' placeholder="City..." name='city' className='city'  />
+                        <input type="text" placeholder='Country code...' name="country" className='country' /><br/>
                         <button value="Search" type="submit">Search</button>
                     </div>
                 </form>
