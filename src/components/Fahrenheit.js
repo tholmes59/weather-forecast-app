e3156b3a38f399d83b8e0eea2a1b3fc562160cef
Thomas <tholmes59@gmail.com>
2020-09-30 09:56:17 -0700

    clean up code

--- src/components/Fahrenheit.js
+++ src/components/Fahrenheit.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 
 const Fahrenheit = (props) => {
-    let imageId = props.weather && props.weather.weather[0].icon
-    let image = `http://openweathermap.org/img/wn/${imageId}@2x.png`
 
     return(
         <>
@@ -13,7 +11,6 @@ const Fahrenheit = (props) => {
                         {props.weather && <span className="max-min-main">{(props.weather.main.temp_max * (9/5)-459.67).toFixed(0)}°F |</span>} {props.weather && <span className="max-min-main">{(props.weather.main.temp_min  * (9/5)-459.67).toFixed(0)}°F</span>}
                     </div>
                     {props.weather && <span className="temp-main">{(props.weather.main.temp * (9/5)-459.67).toFixed(0)}<span className="temp-main-symbol">°F</span> </span>} 
-                    {/* <img src={image} alt=""/> */}
                     {props.weather && <span className="temp-main-feels-like">Feels Like: {(props.weather.main.feels_like * (9/5)-459.67).toFixed(0)}°F</span>}
                 </div>
             }
