9cd0e7c60ddde390bdcf1d27d0c1c588b874b08c
Thomas <tholmes59@gmail.com>
2020-09-30 09:55:14 -0700

    clean up code

--- src/components/Celsius.js
+++ src/components/Celsius.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 
 const Celsius = (props) => {
-    let imageId = props.weather && props.weather.weather[0].icon
-    let image = `http://openweathermap.org/img/wn/${imageId}@2x.png`
-
+   
     return(
         <>
             {props.weather && 
@@ -13,7 +11,6 @@ const Celsius = (props) => {
                         {props.weather && <span className="max-min-main">{(props.weather.main.temp_max - 273.15).toFixed(0)}°C |</span>} {props.weather && <span className="max-min-main">{(props.weather.main.temp_min  - 273.15).toFixed(0)}°C</span>}
                     </div>
                     {props.weather && <span className="temp-main">{(props.weather.main.temp - 273.15).toFixed(0)}<span className="temp-main-symbol">°C</span></span>} 
-                    {/* <img src={image} alt=""/> */}
                     {props.weather && <span className="temp-main-feels-like">Feels Like: {(props.weather.main.feels_like - 273.15).toFixed(0)}°C</span>}
                 </div>
             }
