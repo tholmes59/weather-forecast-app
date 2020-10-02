6cc60feac5af50dcba46ad79d16aaa179d2bc9d8
Thomas <tholmes59@gmail.com>
2020-10-02 09:32:03 -0700

    format code with prettier

--- src/components/Celsius.js
+++ src/components/Celsius.js
@@ -3,19 +3,38 @@ import React from 'react'
 
 const Celsius = (props) => {
    
-    return(
-        <>
-            {props.weather && 
-                <div className="fahrenheit-celsius-container">
-                    <div className="max-min-container">
-                        {props.weather && <span className="max-min-main">{(props.weather.main.temp_max - 273.15).toFixed(0)}°C |</span>} {props.weather && <span className="max-min-main">{(props.weather.main.temp_min  - 273.15).toFixed(0)}°C</span>}
-                    </div>
-                    {props.weather && <span className="temp-main">{(props.weather.main.temp - 273.15).toFixed(0)}<span className="temp-main-symbol">°C</span></span>} 
-                    {props.weather && <span className="temp-main-feels-like">Feels Like: {(props.weather.main.feels_like - 273.15).toFixed(0)}°C</span>}
-                </div>
-            }
-        </>
-    )
+    return (
+      <>
+        {props.weather && (
+          <div className="fahrenheit-celsius-container">
+            <div className="max-min-container">
+              {props.weather && (
+                <span className="max-min-main">
+                  {(props.weather.main.temp_max - 273.15).toFixed(0)}°C |
+                </span>
+              )}{" "}
+              {props.weather && (
+                <span className="max-min-main">
+                  {(props.weather.main.temp_min - 273.15).toFixed(0)}°C
+                </span>
+              )}
+            </div>
+            {props.weather && (
+              <span className="temp-main">
+                {(props.weather.main.temp - 273.15).toFixed(0)}
+                <span className="temp-main-symbol">°C</span>
+              </span>
+            )}
+            {props.weather && (
+              <span className="temp-main-feels-like">
+                Feels Like:{" "}
+                {(props.weather.main.feels_like - 273.15).toFixed(0)}°C
+              </span>
+            )}
+          </div>
+        )}
+      </>
+    );
 }
 
 export default Celsius;
