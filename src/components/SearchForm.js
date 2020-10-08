1deecb2da1324c492126386652adb19210667873
Thomas <tholmes59@gmail.com>
2020-10-08 08:42:42 -0700

    format code with prettier

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,18 +2,30 @@ import React from 'react';
 
 
 const SearchForm = (props) => {
-   
-        return(
-            <div className="search-form">
-                <form onSubmit={props.getWeatherInfo}>
-                    <div className="input-container">
-                        <input type='text' placeholder="City..." name='city' className='city'  />
-                        <input type="text" placeholder='Country code...' name="country" className='country' /><br/>
-                        <button value="Search" type="submit">Search</button>
-                    </div>
-                </form>
-            </div>
-        )
+  return (
+    <div className="search-form">
+      <form onSubmit={props.getWeatherInfo}>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="City..."
+            name="city"
+            className="city"
+          />
+          <input
+            type="text"
+            placeholder="Country code..."
+            name="country"
+            className="country"
+          />
+          <br />
+          <button value="Search" type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SearchForm;
