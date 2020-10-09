693930852c46ea5c6acfb1773fed5dab78c3f233
Thomas <tholmes59@gmail.com>
2020-10-09 09:49:08 -0700

    add cloud div's to app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import PageContainer from './components/PageContainer'
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1>Weather App</h1><br/>
+      <div class="x1">
+        <div class="cloud"></div>
+      </div>
+
+      <div class="x2">
+        <div class="cloud"></div>
+      </div>
+      
       <PageContainer />
     </div>
   );
