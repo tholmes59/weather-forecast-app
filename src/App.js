30f463118a9561ca413d1d2c5bd7cfefedb81809
Thomas <tholmes59@gmail.com>
2020-10-19 10:55:47 -0700

    change name of cloud holder div to better align with its purpose

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App">
       <h1>What's the Weather?</h1><br/>
-      <div className="x1">
+      <div className="cloud1">
         <div className="cloud"></div>
       </div>
 
-      <div className="x2">
+      <div className="cloud2">
         <div className="cloud"></div>
       </div>
       
